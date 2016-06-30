08ab491f121a57f50838ba53822dd7a4bc660e54
Devin O'Daniel <devinodaniel@me.com>
2016-06-30 13:45:29 -0400

    adding users to index output

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-def index
-	@envs = ENV
-end
+	def index
+		@envs = ENV
+		@users = `users`.to_s.split(' ')
+	end
 
 end
